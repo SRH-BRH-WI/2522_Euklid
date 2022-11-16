aba0668636fe56658fc58d5158dc7e6fd8d1a70d
Dr. Dillinger <peter.dillinger@srh.de>
2022-11-16 10:06:51 +0100

    Erste Version vom euklidischen Algorithmus

--- src/Euklid.java
+++ src/Euklid.java
@@ -1,2 +1,20 @@
 public class Euklid {
+    public static void main(String[] args) {
+
+        // Deklararion
+        int a;
+        int b;
+
+        // Initialisierung
+        a = 10;
+        b = 4;
+
+        while ( a != b ) {
+            if ( a > b ) a = a - b;
+            else         b = b - a;
+        }
+
+        System.out.println("a = " + a);
+    }
+
 }
